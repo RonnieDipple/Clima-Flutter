0a52bd6a69a90ccb266e2fbc22991ae7bbe890e3
ronnie <ronniedipple@gmail.com>
2020-10-25 11:18:44 -0600

    http initialized: receiving JSON formatted api response

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,21 +9,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<void> getLocation() async {
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+    print("This line of code triggered");
+  }
+
+  void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
     print(location.longitude);
     print(location.latitude);
   }
 
-  @override
-  void initState() {
-    getLocation();
-    super.initState();
+  void getData() async {
+    http.Response response = await http.get(
+        'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print("Getting a ${response.statusCode} response");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
