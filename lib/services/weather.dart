418d8e1746a7404feb0bc733276846ff6e6eedf6
ronnie <ronniedipple@gmail.com>
2020-11-01 21:47:22 -0600

    passing data through nav

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -3,10 +3,17 @@ import 'package:clima/utilities/api.dart';
 import 'location.dart';
 import 'networking.dart';
 
-const openWeatherMapURL =
-    'http://api.openweathermap.org/data/2.5/weather'; //openWeatherMapURL
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
@@ -15,7 +22,6 @@ class WeatherModel {
         '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
 
     var weatherData = await networkHelper.getData();
-
     return weatherData;
   }
 
